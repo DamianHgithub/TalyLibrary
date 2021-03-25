a8404b505dcb8f2721121d5c414940b2433c4c97
BlasQu <damianhs138@gmail.com>
2021-03-25 15:35:24 +0100

    Taly modified to object* Taly's content functions changed for various types of input.

--- Taly/src/main/java/com/example/taly/Taly.kt
+++ Taly/src/main/java/com/example/taly/Taly.kt
@@ -2,9 +2,21 @@ package com.example.taly
 
 import android.util.Log
 
-class Taly {
-    private val TAG = "TALY"
-    fun m(message: String) {
-        Log.d(TAG, message)
+object Taly {
+    private const val TAG = "LOGTALY"
+    fun m(m: String) {
+        Log.d(TAG, m)
+    }
+    fun m(t: Throwable) {
+        Log.d(TAG, t.toString())
+    }
+    fun m(i: Int) {
+        Log.d(TAG, i.toString())
+    }
+    fun m(l: Long) {
+        Log.d(TAG, l.toString())
+    }
+    fun m(a: Any) {
+        Log.d(TAG, a.toString())
     }
 }

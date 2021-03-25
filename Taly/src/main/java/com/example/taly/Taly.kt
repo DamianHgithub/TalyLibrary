d502a158bb312927de69c6499208106a785469a0
BlasQu <damianhs138@gmail.com>
2021-03-25 21:31:42 +0100

    Changed interface to 2 objects since it gives better performance, final updates to log library

--- Taly/src/main/java/com/example/taly/Taly.kt
+++ Taly/src/main/java/com/example/taly/Taly.kt
@@ -3,33 +3,9 @@ package com.example.taly
 import android.util.Log
 
 object Taly {
-    interface TALY_INTERFACE {
-        fun m(m: String)
-        fun m(t: Throwable)
-        fun m(i: Int)
-        fun m(l: Long)
-        fun m(a: Any)
-    }
-    private val TalyLog: TALY_INTERFACE = object: TALY_INTERFACE {
-        override fun m(m: String) {
-            showMessage(m)
-        }
-        override fun m(t: Throwable) {
-            showMessage(t.toString())
-        }
-        override fun m(i: Int) {
-            showMessage(i.toString())
-        }
-        override fun m(l: Long) {
-            showMessage(l.toString())
-        }
-        override fun m(a: Any) {
-            showMessage(a.toString())
-        }
-    }
     private var tag = "LOGTALY"
-    fun m(m: String) {
-        showMessage(m)
+    fun m(s: String) {
+        showMessage(s)
     }
     fun m(t: Throwable) {
         showMessage(t.toString())
@@ -43,12 +19,29 @@ object Taly {
     fun m(a: Any) {
         showMessage(a.toString())
     }
-    fun tag(tag: String): TALY_INTERFACE {
+    fun tag(tag: String): LogTaly {
         this.tag = tag
-        return TalyLog
+        return LogTaly
     }
-    fun showMessage(message: String) {
+    private fun showMessage(message: String) {
         Log.d(tag, message)
         tag = "LOGTALY"
+    }
+    object LogTaly {
+        fun m(s: String) {
+            showMessage(s)
+        }
+        fun m(t: Throwable) {
+            showMessage(t.toString())
+        }
+        fun m(i: Int) {
+            showMessage(i.toString())
+        }
+        fun m(l: Long) {
+            showMessage(l.toString())
+        }
+        fun m(a: Any) {
+            showMessage(a.toString())
+        }
     }
 }
--- app/src/main/java/com/example/talyexample/MainActivity.kt
+++ app/src/main/java/com/example/talyexample/MainActivity.kt
@@ -2,13 +2,10 @@ package com.example.talyexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.taly.Taly
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Taly.tag("test").m(1)
-        Taly.tag("s").
     }
 }
